c7d2548d91fc06f8a5ff11cbf92d311d7a0a2d7d
Oliver Burnett-Kiernan <oliverbk4@gmail.com>
2025-09-12 17:13:17 +0100

    new node version

--- src/app/pool/page.js
+++ src/app/pool/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 
+
 export default function page() {
     return (
         <main className="flex min-h-screen flex-col items-center  p-4">
