e7d60cf518394e62a18f70824046a06638343761
Oliver Burnett-Kiernan <oliverbk4@gmail.com>
2026-04-24 15:20:46 +0100

    hopefully fixed mobile

--- src/app/process/page.js
+++ src/app/process/page.js
@@ -7,12 +7,13 @@ export default function Projects() {
   return (
     <>
     <Navbar name={"process"} />
-    <main className="bg-white text-text overflow-y-scroll px-8 gap-6 items-center justify-center flex min-h-screen w-full md:w-[70vw] fixed top-0 right-0  flex-col items-center">
-      <p className="text-white rounded-xl text-base w-full bg-black p-1 md:p-4">
+    <main className="bg-white text-text overflow-y-scroll px-8 gap-1 sm:gap-6 items-center justify-center flex min-h-screen w-full md:w-[70vw] fixed top-0 right-0  flex-col items-center">
+      
+      <div className="flex">
+        <div className="flex flex-col mt-4 gap-1 sm:gap-8 lg:gap-20 h-full justify-between">
+          <p className="text-white rounded-xl text-base w-full bg-black p-1 md:p-4">
         This is the standard process I use to design and create a website.  I have developed this over several years of creating websites personally and academically, learning and improving each time.
       </p>
-      <div className="flex">
-        <div className="flex flex-col mt-4 gap-8 lg:gap-20 h-full justify-between">
           <div className="items-center rounded-xl h-full flex  w-full bg-black p-1 sm:p-4">
           <Image alt="Github logo" src="/figmaLogo.png" className="w-[15%] sm:w-[10%] md:w-[5%] mr-4" width={1000} height={1000} objectFit="contain"/>
           <p className="text-white text-base">
